08e8050917e2db1126d01769c3ec68bbd6c65acf
Ryan Davis <ryan@rypac.org>
2023-12-02 20:46:25 +1100

    Implement Day 1 part 2

--- solutions/day-1.hs
+++ solutions/day-1.hs
@@ -1,6 +1,9 @@
 module Main (main) where
 
+import Control.Applicative (asum)
 import Data.ByteString qualified as B
+import Data.List (find)
+import Data.Maybe (mapMaybe)
 import Data.Text qualified as T
 import Data.Text.Encoding (decodeUtf8)
 import System.Environment (getArgs)
@@ -50,7 +53,54 @@ solve1 = sum . fmap extractNumber
 
 -- | The function which calculates the solution for part two
 solve2 :: Input -> Solution
-solve2 = error "Part 2 Not implemented"
+solve2 = sum . mapMaybe extractNumber
+ where
+  extractNumber :: T.Text -> Maybe Int
+  extractNumber line = do
+    firstDigit <- extractFirstDigit line
+    lastDigit <- extractLastDigit line
+    return $ firstDigit * 10 + lastDigit
+
+  extractFirstDigit :: T.Text -> Maybe Int
+  extractFirstDigit = asum . fmap digit . T.tails
+   where
+    digit :: T.Text -> Maybe Int
+    digit line = fmap snd $ find (\a -> fst a `T.isPrefixOf` line) digitMappings
+
+  extractLastDigit :: T.Text -> Maybe Int
+  extractLastDigit = asum . fmap digit . reverseTails
+   where
+    digit :: T.Text -> Maybe Int
+    digit line = fmap snd $ find (\a -> fst a `T.isSuffixOf` line) digitMappings
+
+    reverseTails :: T.Text -> [T.Text]
+    reverseTails text
+      | T.null text = [T.empty]
+      | otherwise = text : reverseTails (T.init text)
+
+  digitMappings :: [(T.Text, Int)]
+  digitMappings =
+    [ ("0", 0)
+    , ("1", 1)
+    , ("2", 2)
+    , ("3", 3)
+    , ("4", 4)
+    , ("5", 5)
+    , ("6", 6)
+    , ("7", 7)
+    , ("8", 8)
+    , ("9", 9)
+    , ("zero", 0)
+    , ("one", 1)
+    , ("two", 2)
+    , ("three", 3)
+    , ("four", 4)
+    , ("five", 5)
+    , ("six", 6)
+    , ("seven", 7)
+    , ("eight", 8)
+    , ("nine", 9)
+    ]
 
 main :: IO ()
 main = do
