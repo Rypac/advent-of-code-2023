8609c13b4820eae81883255a6329eceab414bc74
Ryan Davis <ryan@rypac.org>
2023-12-04 19:33:16 +1100

    Implement Day 4 part 1

--- solutions/day-4.hs
+++ solutions/day-4.hs
@@ -1,7 +1,14 @@
 module Main (main) where
 
 import Data.ByteString qualified as B
+import Data.Set qualified as Set
+import Data.Text qualified as T
+import Data.Text.Encoding (decodeUtf8)
+import Data.Void (Void)
 import System.Environment (getArgs)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as L
 
 {- Types for your input and your solution
 
@@ -9,18 +16,55 @@ import System.Environment (getArgs)
 - Solution should be the type of your solution. Typically is an Int, but It can be other things, like a list of numbers
          or a list of characters
 -}
-type Input = B.ByteString -- default to Bytestring, but very likely you'll need to change it
+type Input = [ScratchCard] -- default to Bytestring, but very likely you'll need to change it
 type Solution = Int
+
+type Parser = Parsec Void T.Text
+
+data ScratchCard = ScratchCard
+  { card :: !Int
+  , numbers :: !(Set.Set Int)
+  , winningNumbers :: !(Set.Set Int)
+  }
+  deriving stock (Show)
 
 -- | parser transforms a raw bytestring (from your ./input/day-X.input) to your Input type.
 --   this is intended to use attoparsec for such a transformation. You can use Prelude's
 --   String if it fit better for the problem
-parser :: B.ByteString -> Input
-parser = undefined
+parser :: String -> B.ByteString -> Input
+parser filepath = parseOrError scratchCards filepath . decodeUtf8
+ where
+  parseOrError :: Parser a -> String -> T.Text -> a
+  parseOrError p path = either (error . errorBundlePretty) id . parse p path
+
+  scratchCards :: Parser [ScratchCard]
+  scratchCards = many scratchCard <* eof
+
+  scratchCard :: Parser ScratchCard
+  scratchCard = do
+    string "Card" >> space
+    card <- number
+    char ':' >> space
+    winningNumbers <- Set.fromList <$> many number
+    char '|' >> space
+    numbers <- Set.fromList <$> many number
+    return $ ScratchCard {..}
+
+  number :: Parser Int
+  number = L.lexeme space L.decimal
 
 -- | The function which calculates the solution for part one
 solve1 :: Input -> Solution
-solve1 = error "Part 1 Not implemented"
+solve1 = sum . fmap cardPoints
+ where
+  cardPoints :: ScratchCard -> Int
+  cardPoints card =
+    countPoints $ Set.size $ Set.intersection card.numbers card.winningNumbers
+   where
+    countPoints :: Int -> Int
+    countPoints 0 = 0
+    countPoints 1 = 1
+    countPoints n = 2 * countPoints (n - 1)
 
 -- | The function which calculates the solution for part two
 solve2 :: Input -> Solution
@@ -32,7 +76,7 @@ main = do
   -- example: cabal run -- day-3 2 "./input/day-3.example"
   -- will run part two of day three with input file ./input/day-3.example
   [part, filepath] <- getArgs
-  input <- parser <$> B.readFile filepath -- use parser <$> readFile filepath if String is better
+  input <- parser filepath <$> B.readFile filepath -- use parser <$> readFile filepath if String is better
   if read @Int part == 1
     then do
       putStrLn "solution to problem 1 is:"
