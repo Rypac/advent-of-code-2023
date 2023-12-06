b0168a1a206cd541bc7da26e6e6bab2f4f702a0b
Ryan Davis <ryan@rypac.org>
2023-12-06 22:16:40 +1100

    Implement Day 5 part 1

--- solutions/Day05.hs
+++ solutions/Day05.hs
@@ -1,16 +1,76 @@
 module Main (main) where
 
 import Data.ByteString qualified as B
+import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8)
+import Data.Void (Void)
 import System.Environment (getArgs)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Text.Megaparsec.Char.Lexer qualified as L
 
-type Input = B.ByteString
+type Input = Almanac
 type Solution = Int
 
-parser :: B.ByteString -> Input
-parser = undefined
+data Almanac = Almanac
+  { seeds :: ![Int]
+  , mappings :: ![Category]
+  }
+  deriving stock (Show)
+
+type Category = [Mapping]
+
+data Mapping = Mapping
+  { source :: !Int
+  , range :: !Int
+  , destination :: !Int
+  }
+  deriving stock (Show)
+
+type Parser = Parsec Void Text
+
+parser :: String -> B.ByteString -> Input
+parser filepath = parseOrError almanac filepath . decodeUtf8
+ where
+  parseOrError :: Parser a -> String -> Text -> a
+  parseOrError p path = either (error . errorBundlePretty) id . parse p path
+
+  almanac :: Parser Almanac
+  almanac = Almanac <$> seeds <*> (many mappings)
+
+  seeds :: Parser [Int]
+  seeds = do
+    string "seeds:" *> space
+    number `sepBy` space
+
+  mappings :: Parser [Mapping]
+  mappings = do
+    _ <- skipManyTill anySingle newline
+    many mapping
+
+  mapping :: Parser Mapping
+  mapping = do
+    destination <- number
+    source <- number
+    range <- number
+    pure $ Mapping {..}
+
+  number :: Parser Int
+  number = L.lexeme space L.decimal
 
 part1 :: Input -> Solution
-part1 = error "Part 1 not implemented"
+part1 almanac = minimum $ fmap (location almanac.mappings) almanac.seeds
+ where
+  location :: [Category] -> Int -> Int
+  location [] seed = seed
+  location (category : rest) seed = location rest (convert category seed)
+
+  convert :: [Mapping] -> Int -> Int
+  convert [] seed = seed
+  convert (Mapping {source, range, destination} : rest) seed =
+    if seed >= source && seed < (source + range)
+      then destination + (seed - source)
+      else convert rest seed
 
 part2 :: Input -> Solution
 part2 = error "Part 2 not implemented"
@@ -18,7 +78,7 @@ part2 = error "Part 2 not implemented"
 main :: IO ()
 main = do
   [part, filepath] <- getArgs
-  input <- parser <$> B.readFile filepath
+  input <- parser filepath <$> B.readFile filepath
   if read @Int part == 1
     then do
       putStrLn "solution to problem 1 is:"
